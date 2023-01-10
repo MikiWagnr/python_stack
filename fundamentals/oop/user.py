7c2fad9d0d0145670f3ba51d7ccaaa32c792d4eb
MikiWagnr <miguelmontilla96@gmail.com>
2023-01-10 18:16:35 -0500

    users updated

--- fundamentals/oop/user.py
+++ fundamentals/oop/user.py
@@ -9,24 +9,30 @@ class User:
     
     def display_info(self):
         print(self.first_name, self.last_name, self.email, self.age, sep= '\n')
+        return self
 
     def enroll(self):
         if self.is_rewards_member == True:
             print('User\'s already a member')
-            return False
+            return self
         else:
             self.is_rewards_member == True
             self.gold_card_points = 200
-            return True
+            return self
 
     def spend_points(self, amount):
         if self.gold_card_points >= amount:
             self.gold_card_points -= amount
-            return self.gold_card_points
         else:
-            return 'insufficient points'
+            print('insufficient funds')
+        return self
 
-id1 = User('Tony', 'Stark', 'iamironman@starkin.com', 48)
-id1.display_info()
-id1.enroll()
-print(id1.spend_points(50))
+# id1 = User('Tony', 'Stark', 'iamironman@starkin.com', 48)
+# id1.display_info()
+# id1.enroll()
+# print(id1.spend_points(50))
+user1 = User('Tony', 'Stark', 'iamironman@starkin.com', 48)
+user1.display_info().enroll().spend_points(50)
+
+user2= User('charles', 'Xavier', 'TomemyXmen@DOE.com', 59)
+user2.display_info().enroll().spend_points(80)
