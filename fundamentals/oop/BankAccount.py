b0bc591c7f540bab68ef68dce71077463e067a72
MikiWagnr <miguelmontilla96@gmail.com>
2023-01-11 23:52:39 -0500

    fixed the class method issues

--- fundamentals/oop/BankAccount.py
+++ fundamentals/oop/BankAccount.py
@@ -4,7 +4,6 @@ class BankAccount:
     def __init__(self, int_rate, balance): 
         self.rate = int_rate
         self.balance = balance
-        # self.user = name
         BankAccount.all_accounts.append(self)
 
 
@@ -31,19 +30,19 @@ class BankAccount:
             print('insufficient funds')
         return self
 
-    # @classmethod
-    # def display_all_accounts(cls):
-    #     for all_accounts in cls.all_accounts:
-    #         print(f"{all_accounts.useracc} has : ${all_accounts.balance} and interest rate is {all_accounts.rate}%")
+    @classmethod
+    def display_all_accounts(cls):
+        for each in cls.all_accounts:
+            print(f"account balance: ${each.balance} and interest rate is: {each.rate}%")
 
-# user1= BankAccount('Jemma', 0.5, 600)
-# user1.deposit(50).deposit(25).deposit(25).withdraw(150).yield_interest().display_account_info()
+user1= BankAccount(0.5, 600)
+user1.deposit(50).deposit(25).deposit(25).withdraw(150).yield_interest().display_account_info().display_all_accounts()
 
-# user2 = BankAccount('Fitz', 0.1, 500)
-# user2.deposit(10).deposit(10).withdraw(50).withdraw(25).withdraw(25).withdraw(50).display_account_info().yield_interest()
+user2 = BankAccount( 0.1, 500)
+user2.deposit(10).deposit(10).withdraw(50).withdraw(25).withdraw(25).withdraw(50).display_account_info().yield_interest().display_all_accounts()
 
 # useracc.display_all_accounts()
-
+#users with bank accounts------------------------------------------------------------------>
 class User:
     def __init__(self, name, email, account):
         self.name = name
@@ -76,5 +75,6 @@ useracc1.make_deposit(500,0).make_withdrawal(50,0).display_user_balance(0)
 
 useracc2 = User('Tony Stark', 'iamironman@starkindustries.com',[BankAccount(int_rate = 0.02, balance = 0), BankAccount(int_rate = 0.1, balance = 1000)])
 useracc2.make_deposit(500,1).make_withdrawal(50,1).display_user_balance(1)
+
 
 useracc1.transfer_money(200, useracc2, 0, 1)
