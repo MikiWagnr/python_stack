d207f6da82c27772d835833d223c80de646c26a8
MikiWagnr <miguelmontilla96@gmail.com>
2023-01-13 22:16:47 -0500

    counter project

---
+++ flask/fundamentals/counter/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = "with great power, there must also come great responsibily"
+
+@app.route('/')
+def counting_views():
+    if 'views' in session:
+        session['views'] += 1
+    else:
+        session['views'] = 0
+    return render_template("index.html")
+
+@app.route('/add2')
+def increase_by2():
+    session['views']+=1
+    return redirect('/')
+
+@app.route('/destroy_session')
+def destroy_session():
+    session.clear()
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True)
