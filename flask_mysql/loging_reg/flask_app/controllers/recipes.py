590afec065d9093ddce759a4ca28ca2c4ba3fdaa
MikiWagnr <miguelmontilla96@gmail.com>
2023-01-23 15:45:35 -0500

    login and reg

---
+++ flask_mysql/loging_reg/flask_app/controllers/recipes.py
@@ -0,0 +1,8 @@
+from flask_app import app, render_template, session, redirect
+
+@app.route('/recipes')
+def recipes():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    else:
+        return render_template('recipes.html')
